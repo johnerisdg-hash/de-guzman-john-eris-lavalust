7ed0cd5b4748c6fcec8e29003259aace1ceb6877
John Eris De Guzman <johnerisdg@gmail.com>
2026-08-19 21:57:51 +0800

    Student-Profile-2

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,7 +44,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-$router->get('/', 'Welcome::index');
+
 
 (function() {
 require APP_DIR . 'config/middleware.php';
